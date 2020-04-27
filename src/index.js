d7d233ab7f60fa8fded0ddb7e8a8fdfd94d98bf3
casey-minzi <cluvhope@gmail.com>
2020-04-27 19:00:34 +0100

    Actions

--- src/index.js
+++ src/index.js
@@ -5,11 +5,22 @@ const subtract = document.getElementById('subtract');
 const number = document.querySelector('span');
 
 //reducer is a function that modifies the data
-const countModifier = (state = 0) => {
-	return state;
+const countModifier = (count = 0, action) => {
+	if (action.type === 'ADD') {
+		return count = count + 1;
+	} else if (action.type === 'SUBTRACT') {
+		return count = count - 1;
+	} else {
+		return count;
+	}
 };
 
 //store stores the data
 const countStore = createStore(countModifier);
 
-console.log(countStore.getState());
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'SUBTRACT' });
+console.log(countStore.getState())
