c20a8be121728e6f3308fac22b6af3b6ffe18670
casey-minzi <cluvhope@gmail.com>
2020-04-28 12:17:07 +0100

    State Mutation

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const ul = document.querySelector('ul');
 const ADD_TODO = 'ADD_TODO';
 const DELETE_TODO = 'DELETE_TODO';
 
+//You should NEVER mutate state. You create new state with previous contents
 const reducer = (state = [], action) => {
-	console.log(action);
 	switch (action.type) {
 		case ADD_TODO :
-			return [];
+			return [...state, { text: action.text, id: Date.now() }];
 		case DELETE_TODO :
 			return [];
 		default :
@@ -19,6 +19,10 @@ const reducer = (state = [], action) => {
 };
 
 const store = createStore(reducer);
+
+//subscribe function takes a callback function called listener.
+//This callback will be invoked everytime dispatch is called
+store.subscribe(() => console.log(store.getState()));
 
 const onSubmit = e => {
 	e.preventDefault();
