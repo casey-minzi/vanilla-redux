1c078377ffd340ca9dcd28049a392bac296d3b62
casey-minzi <cluvhope@gmail.com>
2020-04-28 15:05:11 +0100

    Delete To Do pt 1

--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ const ul = document.querySelector('ul');
 const ADD_TODO = 'ADD_TODO';
 const DELETE_TODO = 'DELETE_TODO';
 
+const addToDo = (text) => {
+	return { type: ADD_TODO, text };
+};
+
+const deleteToDo = (id) => {
+	return { type: DELETE_TODO, id };
+}
 //You should NEVER mutate state. You create new state with previous contents
 const reducer = (state = [], action) => {
 	switch (action.type) {
 		case ADD_TODO :
-			return [...state, { text: action.text, id: Date.now() }];
+			return [{ text: action.text, id: Date.now() }, ...state];
 		case DELETE_TODO :
 			return [];
 		default :
@@ -24,11 +31,37 @@ const store = createStore(reducer);
 //This callback will be invoked everytime dispatch is called
 store.subscribe(() => console.log(store.getState()));
 
+const dispatchAddToDo = (text) => {
+	store.dispatch(addToDo(text))
+};
+
+const dispatchDeleteToDo = (e) => {
+	const id = e.target.parentNode.id;
+	store.dispatch(deleteToDo(id));
+}
+
+const paintToDos = () => {
+	const toDos = store.getState();
+	ul.innerHTML = '';
+	toDos.forEach(toDo => {
+		const li = document.createElement('li');
+		const btn = document.createElement('button');
+		btn.addEventListener('click', dispatchDeleteToDo)
+		btn.innerText = 'Delete';
+		li.id = toDo.id;
+		li.innerText = toDo.text;
+		li.appendChild(btn);
+		ul.appendChild(li);
+	});
+}; 
+
+store.subscribe(paintToDos);
+
 const onSubmit = e => {
 	e.preventDefault();
-	const todo = input.value;
+	const toDo = input.value;
 	input.value = '';
-	store.dispatch({ type: ADD_TODO, text: todo })
+	dispatchAddToDo(toDo);
 };
 
 form.addEventListener('submit', onSubmit);
